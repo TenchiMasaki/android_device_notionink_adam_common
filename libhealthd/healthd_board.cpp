50e5222ed643269cd34af2317dd8cb3733e70791
Tenchi Masaki <tenchimasaki@hotmail.com>
2014-01-31 17:01:25 -0700

    new file:   files/data-local.prop 	new file:   files/p2p_supplicant_overlay.conf 	new file:   files/wpa_supplicant_overlay.conf 	new file:   libhealthd/Android.mk 	new file:   libhealthd/healthd_board.cpp

---
+++ libhealthd/healthd_board.cpp
@@ -0,0 +1,55 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <healthd.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// from BatteryManager.java
+#define BATTERY_STATUS_UNKNOWN 1
+#define BATTERY_STATUS_CHARGING 2
+#define BATTERY_STATUS_DISCHARGING 3
+#define BATTERY_STATUS_NOT_CHARGING 4
+#define BATTERY_STATUS_FULL 5
+
+bool healthd_board_isDockPresent() {
+    int fd = open("/sys/class/power_supply/dock_battery/uevent", O_RDONLY, 0);
+    if (fd == -1) return false;
+
+    const int SIZE = 2;
+    char buf[SIZE];
+    ssize_t count = TEMP_FAILURE_RETRY(read(fd, buf, SIZE));
+
+    close(fd);
+    return count == SIZE;
+}
+
+void healthd_board_init(struct healthd_config *config)
+{
+    config->batteryStatusPath = "/sys/class/power_supply/battery/status";
+    config->batteryHealthPath = "/sys/class/power_supply/battery/health";
+    config->batteryPresentPath = "/sys/class/power_supply/battery/present";
+    config->batteryCapacityPath = "/sys/class/power_supply/battery/capacity";
+    config->batteryVoltagePath = "/sys/class/power_supply/battery/voltage_now";
+    config->batteryTemperaturePath = "/sys/class/power_supply/battery/temp";
+    config->batteryTechnologyPath = "/sys/class/power_supply/battery/technology";
+}
+
+int healthd_board_battery_update(struct android::BatteryProperties *props)
+{
+    // board doesn't have the present sysfs for dock battery, so we need to
+    // fill it from the uevent sysfs. If the uevent sysfs have some data then
+
+    // To follow the AOSP battery contract, the board only should report itself
+    // as pluggable when is charging or full charging
+    if (props->batteryStatus != BATTERY_STATUS_CHARGING &&
+        props->batteryStatus != BATTERY_STATUS_FULL) {
+
+        props->chargerAcOnline = false;
+        props->chargerUsbOnline = false;
+        props->chargerWirelessOnline = false;
+    }
+    // return 0 to log periodic polled battery status to kernel log
+    return 0;
+}
+
+
