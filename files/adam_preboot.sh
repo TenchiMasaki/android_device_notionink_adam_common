fcf8adaeafb20d56f23dce9212575f7d073f1a64
Tenchi Masaki <tenchimasaki@hotmail.com>
2015-02-03 11:04:00 -0700

    preboot changes

--- files/adam_preboot.sh
+++ files/adam_preboot.sh
@@ -6,16 +6,17 @@
 # Disable TCP/IP v6
 #sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
-rm /data/aplog
+mv /data/aplog /data/last_aplog
 /system/bin/logcat -f /data/aplog &
 dmesg > /data/dmsg &
+#mv /data/kmsg /data/last_kmsg
 #cat /proc/kmsg > /data/kmsg &
 
 # Sleep freeze workaround (makes usb unstable, but suspend power usage less)
     #echo 1 > /sys/module/cpuidle/parameters/lp2_in_idle
 
 # Alternate sleep freeze workaround to keep cpu awake (doesn't affect usb, but more power usage)
-    echo keepawake > /sys/power/wake_lock # some Adams cannot deep sleep
+    #echo keepawake > /sys/power/wake_lock # some Adams cannot deep sleep
 
 # Interactive Governor Settings   (write cpufreq before cpu0,cpu1)
     echo 216000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq # Minimum clock speed
