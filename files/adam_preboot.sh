0ee774121443a991718265f2b3b2492e0a8126a7
Tenchi Masaki <tenchimasaki@hotmail.com>
2015-02-08 08:45:08 -0700

    adjust clocks

--- files/adam_preboot.sh
+++ files/adam_preboot.sh
@@ -19,11 +19,11 @@ dmesg > /data/dmsg &
     #echo keepawake > /sys/power/wake_lock # some Adams cannot deep sleep
 
 # Interactive Governor Settings   (write cpufreq before cpu0,cpu1)
-    echo 216000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq # Minimum clock speed
-    echo 1504000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq # ** Set 1.6Ghz nax clock speed **
+    echo 100000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq # Minimum clock speed
+    echo 1500000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq # ** Set 1.6Ghz nax clock speed **
     echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # Set governor
-    echo 216000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq # Minimum clock speed for second core
-    echo 1504000 >/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq # Max clock speed for second core
+    echo 100000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq # Minimum clock speed for second core
+    echo 1500000 >/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq # Max clock speed for second core
     echo interactive > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor # Set governor for second core
     echo 80 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
     echo 1 > /sys/devices/system/cpu/cpufreq/interactive/input_boost
