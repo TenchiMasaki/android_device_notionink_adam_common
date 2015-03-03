66fd1a1bb164343d86d633a49933341921e1d826
Tenchi Masaki <tenchimasaki@hotmail.com>
2015-03-03 13:37:53 -0700

    update clocks and sio

--- files/adam_preboot.sh
+++ files/adam_preboot.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
 # Enable PixelQi mode on boot
-    #echo 1 > /sys/devices/platform/pwm-backlight/PQiModeOn
+#    echo 1 > /sys/devices/platform/pwm-backlight/PQiModeOn
 
 # Disable TCP/IP v6
 #sysctl -w net.ipv6.conf.default.disable_ipv6=1
@@ -20,10 +20,10 @@ dmesg > /data/dmsg &
 
 # Interactive Governor Settings   (write cpufreq before cpu0,cpu1)
     echo 216000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq # Minimum clock speed
-    echo 1504000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq # ** Set 1.6Ghz nax clock speed **
+    echo 1500000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq # ** Set 1.6Ghz nax clock speed **
     echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # Set governor
     echo 216000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq # Minimum clock speed for second core
-    echo 1504000 >/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq # Max clock speed for second core
+    echo 1500000 >/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq # Max clock speed for second core
     echo interactive > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor # Set governor for second core
     echo 80 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
     echo 1 > /sys/devices/system/cpu/cpufreq/interactive/input_boost
@@ -35,8 +35,8 @@ dmesg > /data/dmsg &
     echo 256 > /sys/block/mtdblock4/queue/read_ahead_kb
 
 # Set I/O scheduler noop deadline cfq sio bfq row
-    echo noop > /sys/block/mmcblk0/queue/scheduler
-    echo noop > /sys/block/mmcblk1/queue/scheduler
-    echo noop > /sys/block/mtdblock3/queue/scheduler
-    echo noop > /sys/block/mtdblock4/queue/scheduler
+    echo sio > /sys/block/mmcblk0/queue/scheduler
+    echo sio > /sys/block/mmcblk1/queue/scheduler
+    echo sio > /sys/block/mtdblock3/queue/scheduler
+    echo sio > /sys/block/mtdblock4/queue/scheduler
 
