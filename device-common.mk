11df068a5b2a838d4b3a1616f37b186b4e4cce0a
tenchimasaki <tenchimasaki@hotmail.com>
2015-03-21 14:29:50 -0600

    pa 5.1

--- device-common.mk
+++ device-common.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES += \
 #   device/notionink/adam_common/files/apns-conf.xml:system/etc/apns-conf.xml
 
+# Boot animation
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/bootanimation/res/800x480.zip:system/media/bootanimation.zip
+
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     ro.sf.lcd_density=120
