665a902e0d22febe56a562c5bc5ae7e7bb1f51e9
Tenchi Masaki <tenchimasaki@hotmail.com>
2016-06-11 10:36:52 -0600

    update settings

--- device-common.mk
+++ device-common.mk
@@ -28,9 +28,9 @@ endif
 DEVICE_PACKAGE_OVERLAYS := device/notionink/adam_common/overlay
 
 # uses mdpi artwork where available
-PRODUCT_AAPT_CONFIG := normal mdpi hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
-PRODUCT_LOCALES += en mdpi
+# PRODUCT_AAPT_CONFIG := normal mdpi hdpi xhdpi
+# PRODUCT_AAPT_PREF_CONFIG := mdpi
+# PRODUCT_LOCALES += en mdpi
 
 # Enable optional sensor types
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -67,13 +67,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Adam/Harmony Configs
 PRODUCT_COPY_FILES := \
     $(LOCAL_KERNEL):kernel \
-    device/notionink/adam_common/sdcard-boot/fstab.harmony:root/fstab.harmony \
-    device/notionink/adam_common/sdcard-boot/fstab.harmony:root/fstab.unknown \
-    device/notionink/adam_common/sdcard-boot/init.harmony.rc:root/init.harmony.rc \
-    device/notionink/adam_common/sdcard-boot/init.harmony.rc:root/init.unknown.rc \
+    device/notionink/adam_common/files/fstab.harmony:root/fstab.harmony \
+    device/notionink/adam_common/files/init.harmony.rc:root/init.harmony.rc \
     device/notionink/adam_common/files/init.harmony.usb.rc:root/init.harmony.usb.rc \
     device/notionink/adam_common/files/ueventd.harmony.rc:root/ueventd.harmony.rc \
-    device/notionink/adam_common/files/ueventd.harmony.rc:root/ueventd.unknown.rc \
     device/notionink/adam_common/files/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/notionink/adam_common/files/nvram.txt:system/etc/wifi/nvram.txt \
     device/notionink/adam_common/files/adam_preboot.sh:system/etc/adam_preboot.sh \
@@ -140,10 +137,6 @@ PRODUCT_COPY_FILES += \
 # APNs list
 #PRODUCT_COPY_FILES += \
 #   device/notionink/adam_common/files/apns-conf.xml:system/etc/apns-conf.xml
-
-# Boot animation
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bootanimation/halfres/600.zip:system/media/bootanimation.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
@@ -241,7 +234,8 @@ PRODUCT_PACKAGES += \
 	libwebkit \
 	libmmcamera_interface2 \
 	libmmcamera_interface \
-	Launcher3
+	Launcher3 \
+        Snap
     
 # Sensor daemon
 PRODUCT_PACKAGES += \
@@ -308,6 +302,7 @@ PRODUCT_PACKAGES += \
 
 # Filesystem management tools and others
 PRODUCT_PACKAGES += \
+	badblocks \
 	e2fsck \
 	setup_fs \
         make_ext4fs \
