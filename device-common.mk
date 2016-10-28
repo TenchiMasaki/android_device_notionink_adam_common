fc8dbb8a0e9792d4cfadc3aa4285a9d303eaced9
Tenchi Masaki <tenchimasaki@hotmail.com>
2016-10-28 17:58:48 -0600

    Fix codecs

--- device-common.mk
+++ device-common.mk
@@ -125,8 +125,7 @@ PRODUCT_COPY_FILES += \
      device/notionink/adam_common/files/media_codecs.xml:system/etc/media_codecs.xml \
      frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
      frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
-#    device/notionink/adam_common/files/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    device/notionink/adam_common/files/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Mixer paths
 PRODUCT_COPY_FILES += \
