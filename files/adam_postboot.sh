3f165bc7106466a51e7adfeabc4474766e12f62b
Tenchi Masaki <tenchimasaki@hotmail.com>
2017-03-10 16:00:45 -0700

    updates

--- files/adam_postboot.sh
+++ files/adam_postboot.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-echo 1 > /sys/block/zram0/reset
+#echo 1 > /sys/block/zram0/reset
 # zram size 500 mb
-echo 500000000 > /sys/block/zram0/disksize
-mkswap /dev/block/zram0
-swapon /dev/block/zram0
+#echo 500000000 > /sys/block/zram0/disksize
+#mkswap /dev/block/zram0
+#swapon /dev/block/zram0
