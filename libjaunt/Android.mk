600e2e83323c4e2704b5ebe9ebd689a149aa6a94
Tenchi <tenchimasaki@github.com>
2017-10-11 17:11:55 -0600

    libjaunt from Decatf

---
+++ libjaunt/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	jaunt.c
+
+LOCAL_CFLAGS += -Werror -mcpu=cortex-a9 -mfloat-abi=softfp -mfpu=vfpv3-d16
+LOCAL_CFLAGS += -fvisibility=hidden
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+LOCAL_MODULE := libjaunt
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES:= libutils libbinder libui liblog libcutils libmedia libandroid_runtime libhardware_legacy libc libstdc++ libm libandroid
+
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ libjaunt/jaunt.c
@@ -0,0 +1,126 @@
+/*
+ * libjaunt
+ * Copyright (C) 2017 <decatf@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define LOG_TAG "libjaunt"
+
+#include <dlfcn.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <ucontext.h>
+#include <pthread.h>
+#include <cutils/log.h>
+#include <sys/prctl.h>
+
+#include "jaunt.h"
+
+void* libtcg_arm_handle = NULL;
+static int initialized = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int (*init_tcg_arm)(void);
+void (*exec)(
+	uint32_t *regs, uint64_t* fpregs,
+    uint32_t *cpsr, uint32_t *fpscr,
+    uint32_t *fpexc,
+    int dump_reg);
+
+void init_arm_tcg_lib(void)
+{
+	int res;
+
+	if (likely(libtcg_arm_handle != NULL))
+		return;
+
+	pthread_mutex_lock(&mutex);
+
+	void *handle = dlopen("libtcg_arm.so", RTLD_NOW | RTLD_LOCAL);
+	if (!handle) {
+		ALOGE("Error loading libtcg_arm.so: %s", dlerror());
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
+
+	init_tcg_arm = dlsym(handle, "init_tcg_arm");
+	exec = dlsym(handle, "exec");
+
+	if (init_tcg_arm == NULL || exec == NULL) {
+		ALOGE("Error loading symbols from libtcg_arm.so");
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
+
+	res = init_tcg_arm();
+
+	if (!res) {
+		libtcg_arm_handle = handle;
+		initialized = 1;
+	} else {
+		ALOGE("init_tcg_arm() returned %d", res);
+	}
+
+	pthread_mutex_unlock(&mutex);
+}
+
+inline int check_vfp_magic(struct vfp_sigframe *vfp) {
+	return vfp->magic == VFP_MAGIC;
+}
+
+void sig_handler(int signal, siginfo_t *info, void *context) {
+	(void) info;
+	struct sigcontext *uc_mcontext = &((ucontext_t*)context)->uc_mcontext;
+	struct aux_sigframe *aux = (struct aux_sigframe*)&((ucontext_t*)context)->uc_regspace;
+	struct vfp_sigframe *vfp = (struct vfp_sigframe*)aux;
+
+	if (signal != SIGILL && info->si_code != ILL_ILLOPC) return;
+
+	/* One time init*/
+	init_arm_tcg_lib();
+
+	if (unlikely(!initialized)) {
+		ALOGE("ARM emulation unit not initialized.\n");
+		abort();
+	}
+
+	if (unlikely(!check_vfp_magic(vfp))) {
+		ALOGE("VFP Magic check failed. magic = 0x%lX\n", vfp->magic);
+		abort();
+	}
+
+	exec(
+		(uint32_t*)&uc_mcontext->arm_r0,
+		(uint64_t*)vfp->ufp.fpregs,
+		(uint32_t*)&uc_mcontext->arm_cpsr,
+		(uint32_t*)&vfp->ufp.fpscr,
+		(uint32_t*)&vfp->ufp_exc.fpexc,
+		0);
+}
+
+struct sigaction old_sa, new_sa = {
+	.sa_flags     = SA_SIGINFO,
+	.sa_sigaction = &sig_handler };
+
+void __attribute__ ((constructor)) init(void) {
+	sigaction(SIGILL, &new_sa, &old_sa);
+}
+
+void __attribute__ ((destructor)) finish(void) {
+	sigaction(SIGILL, &old_sa, &new_sa);
+}
